90bae63464a6ae4bde4a3fe17d1736a53ecb865d
Mark <mark.omuga@student.moringaschool.com>
2023-10-25 11:31:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(roller_skate = "roller-skate")
+{
+	return roller_skate;
+}
+
+function mondayWork(roller_skate = "go to the office")
+{
+	return roller_skate;
+}
+
+function wrapAdjective(punctuation = "*")
+{
+	return function (adjective = "special") {
+		return `You are ${punctuation}${adjective}`;
+	};
+}
